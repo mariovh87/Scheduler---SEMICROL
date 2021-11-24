ba2fc6e92cab0c67b7737ceed7ae655b8a7db35c
Mario Viadero Hidalgo <viadero_live@hotmail.com>
2021-11-24 16:52:28 +0100

    GetDateInMonth,GetWeekdaysForMonth, GetDaysInMonth methods

--- Scheduler.Application/UseCases/MonthlyRecurrenceCalculator.cs
+++ Scheduler.Application/UseCases/MonthlyRecurrenceCalculator.cs
@@ -4,11 +4,53 @@ using Semicrol.Scheduler.Domain.Entities;
 using Semicrol.Scheduler.Domain.Entities.MonthlyConfiguration;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using static Semicrol.Scheduler.Domain.Common.SchedulerEnums;
 
 namespace Semicrol.Scheduler.Application.UseCases
 {
     public static class MonthlyRecurrenceCalculator
     {
+        public static IList<DateTime> GetTheCheckRecurrences(DateTime currentDate, Limits limits, MonthlyConfiguration config)
+        {
+            DateTimeExtensionMethods.EnsureDateIsInRange(currentDate, limits.StartDate, limits.EndDate);
+            Ensure.That(config).IsNotNull();
+            Ensure.That(config.The).IsTrue();
+            List<DateTime> recurrenceDays = new();
+
+
+            return recurrenceDays;
+        }
+
+        public static DateTime GetDateInMonth(IEnumerable<DateTime> days, MonthlyFrecuency frecuency)
+        {
+            Ensure.That(days).IsNotNull();
+            Ensure.That(days.Any()).IsTrue();
+            switch (frecuency)
+            {
+                case MonthlyFrecuency.First:return days.FirstOrDefault();
+                case MonthlyFrecuency.Second: return days.ElementAtOrDefault(1);
+                case MonthlyFrecuency.Third: return days.ElementAtOrDefault(2);
+                case MonthlyFrecuency.Fourth: return days.ElementAtOrDefault(3);
+                case MonthlyFrecuency.Last: return days.LastOrDefault();
+            }
+            return DateTime.MinValue;
+        }
+
+        public static IEnumerable<DateTime> GetWeekdaysForMonth(DateTime currentDate, DayOfWeek weekDay)
+        {
+            return GetDaysInMonth(currentDate, 1).Where(day => day.DayOfWeek == weekDay);
+        }
+        private static IEnumerable<DateTime> GetDaysInMonth(DateTime date, int startDay)
+        {
+            var dateStart = new DateTime(date.Year, date.Month, startDay);
+            while (dateStart.Month == date.Month)
+            {
+                yield return dateStart;
+                dateStart = dateStart.AddDays(1);
+            }
+        }
+
         public static IList<DateTime> GetDayRecurrences(DateTime currentDate, Limits limits, MonthlyConfiguration config)
         {
             DateTimeExtensionMethods.EnsureDateIsInRange(currentDate, limits.StartDate, limits.EndDate);
@@ -48,7 +90,7 @@ namespace Semicrol.Scheduler.Application.UseCases
             }
             DateTime startDate = currentDate;
             startDate = startDate.AddMonths(everyMonths);
-            while (startDate < limitEndDate)
+            if (startDate < limitEndDate)
             {              
                 if (CheckIfMonthHaveDay(startDate, everyDay))
                 {
@@ -58,7 +100,6 @@ namespace Semicrol.Scheduler.Application.UseCases
                 {
                     return new(startDate.Year, startDate.Month, DateTime.DaysInMonth(startDate.Year, startDate.Month));
                 }
-                startDate = startDate.AddMonths(everyMonths);
             }
             
             return DateTime.MaxValue;
